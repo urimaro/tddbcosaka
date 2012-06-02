346933d29b9a14dc14622f8981bf2a5ddfab73f8
Motoki Ito <gnnk@gnnk.net>
2012-06-02 15:02:12 +0900

    Use subject

--- jihanki_spec.rb
+++ jihanki_spec.rb
@@ -8,15 +8,18 @@ describe "自販機" do
   before do
     @jihanki = Jihanki.new
   end
+  
+  subject { @jihanki.display }
+
   context "何もしないときに0円を表示" do
-    it { @jihanki.display.should == 0 }
+    it { should == 0 }
   end
 
   context "自販機に入る場合" do
     [10, 50, 100, 500, 1000].each do |num|
       context "#{num}円を入力したら#{num}円と表示" do
         before { @jihanki.input(num) }
-        it { @jihanki.display.should == num }
+        it { should eq num }
       end
     end
   end
@@ -25,7 +28,7 @@ describe "自販機" do
     [1, 5, 2000, 5000, 10000].each do |num|
       context "#{num}円を入力したら0円と表示" do
         before { @jihanki.input(num) }
-        it { @jihanki.display.should == 0 }
+        it { should eq 0 }
       end
     end
   end
