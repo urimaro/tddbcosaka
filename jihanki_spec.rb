1f31cdb4aceb743ee586a2d1a90e6b5f7e3d65bf
Motoki Ito <gnnk@gnnk.net>
2012-06-02 13:12:19 +0900

    Use each

--- jihanki_spec.rb
+++ jihanki_spec.rb
@@ -13,56 +13,20 @@ describe "自販機" do
   end
 
   context "自販機に入る場合" do
-    context "10円を入力したら10円と表示" do
-      before { @jihanki.input(10) }
-      it { @jihanki.display.should == 10 }
-    end
-
-    context "50円を入力したら50円と表示" do
-      before { @jihanki.input(50) } 
-      it { @jihanki.display.should == 50 }
-    end
-
-    context "100円を入力したら100円と表示" do
-      before { @jihanki.input(100) }
-      it { @jihanki.display.should == 100 }
-    end
-
-    context "500円を入力したら500円と表示" do
-      before { @jihanki.input(500) }
-      it { @jihanki.display.should == 500 }
-    end
-
-    context "1000円を入力したら1000円と表示" do
-      before { @jihanki.input(1000) }
-      it { @jihanki.display.should == 1000 }
+    [10, 50, 100, 500, 1000].each do |num|
+      context "#{num}円を入力したら#{num}円と表示" do
+        before { @jihanki.input(num) }
+        it { @jihanki.display.should == num }
+      end
     end
   end
   
   context "自販機に入らない場合" do
-    context "1円を入力したら0円と表示" do
-      before { @jihanki.input(1) }
-      it { @jihanki.display.should == 0 }
-    end
-
-    context "5円を入力したら0円と表示" do
-      before { @jihanki.input(5) }
-      it { @jihanki.display.should == 0 }
-    end
-
-    context "2000円を入力したら0円と表示" do
-      before { @jihanki.input(2000) }
-      it { @jihanki.display.should == 0 }
-    end
-
-    context "5000円を入力したら0円と表示" do
-      before { @jihanki.input(5000) }
-      it { @jihanki.display.should == 0 }
-    end
-
-    context "10000円を入力したら0円と表示" do
-      before { @jihanki.input(10000) }
-      it { @jihanki.display.should == 0 }
+    [1, 5, 2000, 5000, 10000].each do |num|
+      context "#{num}円を入力したら0円と表示" do
+        before { @jihanki.input(num) }
+        it { @jihanki.display.should == 0 }
+      end
     end
   end
 end
