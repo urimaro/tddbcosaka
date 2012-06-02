6eeed2bcd8afc791ad2571e2c039daf0747cf726
Goh Matsumoto <urimaro@gmail.com>
2012-06-02 15:48:51 +0900

    delete metnods:display, getOtsuri

--- jihanki.rb
+++ jihanki.rb
@@ -1,23 +1,17 @@
 class Jihanki
   def initialize
-    @money = 0
+    @total = 0
     @otsuri = 0
   end
 
-  def total
-    @money
-  end
+  attr_accessor :total, :otsuri
   
   def input(input_money)
     case input_money
     when 1, 5, 2000, 5000, 10000
       @otsuri += input_money
     when 10, 50 ,100, 500, 1000
-      @money += input_money
+      @total += input_money
     end
-  end
-
-  def otsuri
-    @otsuri
   end
 end
